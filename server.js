22ca9c0cca799969faa1590d4445e31c146551f0
Islom <karimov.islam5828859@gmail.com>
2024-11-14 03:11:42 +0900

    views and server.js were modified and POST request is learnt

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.set("view engine", "ejs"); //view engine ejs ligini ko'rsatib beryabmiz
 
 
 //4 **Router'larga** mo'ljallangan
+/*
 app.get("/", function(req, res){
     res.end(`<h1 style="background: red" >HELLO WORLD</h1>`);
 });
@@ -28,6 +29,22 @@ app.get("/gift", function(req, res){
     res.end(`<h1 style="background: green" >GIFT PAGE</h1>`);
 });
 
+*/
+
+
+//app request 3 qismdan iborat: 1) URL qismi (start line), HTTP request header, Request Body
+app.post("/create-item", (req, res)=>{ //post o'zi bilan data olib kelib databasega yozadi
+    console.log(req.body); //kelgan requestni body'sini tekshiryabmiz
+    res.json({test: "success"});  //json shakilda ma'lumotni qaytarib yuboryabmiz
+})
+
+app.get("/", function(req, res){ //get database'dan ma'lumot o'qiydi
+    res.render("harid"); //ejs dagi harid.ejs fileni render qilyabmiza
+});
+
+
+
+
 const server = http.createServer(app); //serverga appni yuklayabmiza
 let PORT = 3000;
 server.listen(PORT, function(){
@@ -37,5 +54,5 @@ server.listen(PORT, function(){
 //express orqali web server qurib oldik.
 //front end'ni qurish 2 usuli bor 1) Backend'da front qurib olish BSSR using EJS 2) React - single page rendering
 
-
+//git clean -df oxirgi ruchnoy kiritgan narsalarni udalit qiladi
 

6e5931714a99d58018b6ac3dd6ea84530a8fa75a
Islom <karimov.islam5828859@gmail.com>
2024-11-15 23:17:58 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const app = express(); //instance yasash uchun kerak bo'ladi app
 
 //1 **KIRISH** expressga kirib kelgan codelar yoziladi
 app.use(express.static("public"));    //har qanday browserdan kirib kelyotgan zaproslar uchun public folder ochiq degan manoni beradi
+//MiddleWare DB
 app.use(express.json())               //kirib kelyotgan json formatdagi datani obj holatga o'girib beradi
+//MiddleWare DB => REST api uchun xizmat qiladi
 app.use(express.urlencoded({extended: true}));        //html dan traditional request (form request) dan express serverimiz qabul qivolamiza. Malasan HTML form post shaklida kevotgan requestlar
+//MiddleWare DB
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data)=>{
+    if(err){
+        console.log("ERROR", err)
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 //2 **Session CODE**
 
@@ -16,27 +29,17 @@ app.set("view engine", "ejs"); //view engine ejs ligini ko'rsatib beryabmiz
 
 
 //4 **Router'larga** mo'ljallangan
-/*
-app.get("/", function(req, res){
-    res.end(`<h1 style="background: red" >HELLO WORLD</h1>`);
-});
-
-app.get("/hello", function(req, res){
-    res.end(`<h1 style="background: orange" >HELLO MIT</h1>`);
-});
-
-app.get("/gift", function(req, res){
-    res.end(`<h1 style="background: green" >GIFT PAGE</h1>`);
-});
-
-*/
-
-
 //app request 3 qismdan iborat: 1) URL qismi (start line), HTTP request header, Request Body
 app.post("/create-item", (req, res)=>{ //post o'zi bilan data olib kelib databasega yozadi
     console.log(req.body); //kelgan requestni body'sini tekshiryabmiz
     res.json({test: "success"});  //json shakilda ma'lumotni qaytarib yuboryabmiz
 })
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+});
+
+
 
 app.get("/", function(req, res){ //get database'dan ma'lumot o'qiydi
     res.render("harid"); //ejs dagi harid.ejs fileni render qilyabmiza
@@ -55,4 +58,15 @@ server.listen(PORT, function(){
 //front end'ni qurish 2 usuli bor 1) Backend'da front qurib olish BSSR using EJS 2) React - single page rendering
 
 //git clean -df oxirgi ruchnoy kiritgan narsalarni udalit qiladi
+
+//FRONTEND DEVELOPMENT: BSSR (EJS) & Single Page Rendering (REACT)
+
+/* 
+API REQUEST:
+    TYPEs: =>        Traditional API | REST API | GraphQL API
+    STRUCTURE: =>    Header | Body
+    METHODs: =>      get | post
+*/
+
+// PATTERN (qolib): Architecture (BUTUN UCHASTKA UCHUN) & Design (SPECIFIC UCHASTKA UCHUN JAVOB BERADI)
 

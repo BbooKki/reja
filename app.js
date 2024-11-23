fa5e305294fbd43bba5bd8067a88e2f5e5383222
Islom <karimov.islam5828859@gmail.com>
2024-11-23 21:24:28 +0900

    Fixed code

--- app.js
+++ app.js
@@ -1,87 +1,52 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express(); //instance yasash uchun kerak bo'ladi app
-const res = require("express/lib/response");
-const fs = require("fs");
-
-
-
+console.log('Web Serverni boshlash');
+const express = require("express"); 
+const app = express(); 
 
 //MongoDB chaqirish
-const db = client.db();
+const db = require("./server").db();
 
+// *****1 - Kirish codelari*******
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+// ********2 - Sessionga bogliq bolgan codelar*******
+// ********3 - Views ga bogliq codelar*********
 
-//Front
-// Design Pattern lar 2 xil bo'ladi Architectual va Design Patternlar
-//1 **KIRISH** expressga kirib kelgan codelar yoziladi
-app.use(express.static("public"));    //har qanday browserdan kirib kelyotgan zaproslar uchun public folder ochiq degan manoni beradi
-//MiddleWare DP
-
-app.use(express.json())               //kirib kelyotgan json formatdagi datani obj holatga o'girib beradi
-//MiddleWare DP => REST api uchun xizmat qiladi
-// REST API
-
-app.use(express.urlencoded({extended: true}));        //html dan traditional request (form request) dan express serverimiz qabul qivolamiza. Malasan HTML form post shaklida kevotgan requestlar
-// TRADITIONAL API
-
-//MiddleWare DP
-
-
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data)=>{
-    if(err){
-        console.log("ERROR", err)
-    } else {
-        user = JSON.parse(data);
-    }
-});
-
-//2 **Session CODE**
-
-//3 **VIEWS CODE** Backenda front end yasaymiz (VIEW yasaymiz)
-//EJS backend'da front end'mizni qurub beryabdi
 app.set("views", "views");
-app.set("view engine", "ejs"); //view engine ejs ligini ko'rsatib beryabmiz
+app.set("view engine", "ejs"); 
 
-
-//4 **Router'larga** mo'ljallangan
-//app request 3 qismdan iborat: 1) URL qismi (start line), HTTP request header, Request Body
-app.post("/create-item", (req, res)=>{ //post o'zi bilan data olib kelib databasega yozadi
-    console.log('user entered /create-item'); //kelgan requestni body'sini tekshiryabmiz
+// *****4 - Rooting ga bogliq codelar********
+app.post("/create-item", (req, res) => {
+    console.log('user entered /create-item');
+    console.log(req.body);
+    // res.end("success");
     const new_reja = req.body.reja;
-    db.collection("plans").insertOne({reja: new_reja}, (err, data)=>{
-        if(err){
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
             console.log(err);
-            res.end("something went wrong");
-        }else {
-            res.end("successfully added");
+            res.end("Something went wrong")
+        } else {
+            console.log(new_reja);
+            res.end("Successfully added")
         }
-    }); //json shakilda ma'lumotni qaytarib yuboryabmiz
+    })
 })
 
-app.get('/author', (req, res) => {
-    db.collection("plans").find().toArray()
-    res.render("author", {user: user});
-});
-
-
-
-app.get("/", function(req, res){ //get database'dan ma'lumot o'qiydi
+app.get("/", function(req, res) {
     console.log('user entered /');
-    db.collection("plans").
-    find().
-    toArray((err, data) =>{
-        if(err){
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err) {
             console.log(err);
             res.end("something went wrong");
         } else {
-            res.render("reja", {items: data});
+            // console.log(data);
+            res.render("reja", { items: data });
         }
-    })
-
-    res.render("reja"); //ejs dagi harid.ejs fileni render qilyabmiza
-});
+    });
+})
 
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,44 +1,24 @@
 const http = require("http");
-const app = require("./app");
 const mongodb = require("mongodb");
 
-
 let db;
-const connectionString = "mongodb+srv://karimovislam5828859:hakim5828859@cluster0.wnfdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const connectionString = "mongodb+srv://karimovislam5828859:hakim5828859@cluster0.wnfdz.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
 
 mongodb.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, (err, client)=>{
-    if(err) console.log("Error on connection MongoDB");
-    else{
-        console.log("MongoDB connection succeed"); 
-        const app = require("./app");
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, (err, client) => {
+    if(err) console.log("ERROR:", err);
+    else {
+        console.log("MongoDB connection succeed");
+        // console.log(client);
         module.exports = client;
 
-        const server = http.createServer(app); //serverga appni yuklayabmiza
+        const app = require("./app");
+        const server = http.createServer(app);
         let PORT = 3000;
-        server.listen(PORT, function(){
-            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-        });
+        server.listen(PORT, function() {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`)
+        })
     }
 });
-
-
-
-
-//express orqali web server qurib oldik.
-//front end'ni qurish 2 usuli bor 1) Backend'da front qurib olish BSSR using EJS 2) React - single page rendering
-
-//git clean -df oxirgi ruchnoy kiritgan narsalarni udalit qiladi
-
-//FRONTEND DEVELOPMENT: BSSR (EJS) & Single Page Rendering (REACT)
-
-/* 
-API REQUEST:
-    TYPEs: =>        Traditional API | REST API | GraphQL API
-    STRUCTURE: =>    Header | Body
-    METHODs: =>      get | post
-*/
-
-// PATTERN (qolib): Architecture (BUTUN UCHASTKA UCHUN) & Design (SPECIFIC UCHASTKA UCHUN JAVOB BERADI)

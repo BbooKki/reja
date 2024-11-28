e04831d35e8845b205df4277179f208f1331089e
Islom <karimov.islam5828859@gmail.com>
2024-11-28 17:17:21 +0900

    Train.js tasklar tepaga o'tdi

--- train.js
+++ train.js
@@ -1,88 +1,41 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba bo'ling",
-    "to'g'ri boshliq tanlang",
-    "uzingizga ishlashni boshlang",
-    "siz kuchli bolgan narsalarni qiling",
-    "yoshlarga investitsiya qiling",
-    "endi dam oling",
-];
 
-//CALLBACK FUNCTION
+/*
 
-function maslahatBering(a, callback){
-    if(typeof a !== "number") callback("insert number", null);
-    else if(a <= 20)  return list[0];
-    else if(a > 20 && a <= 30) return list[1];
-    else if(a > 30 && a <= 40) return list[2];
-    else if(a > 40 && a <= 50) return list[3];
-    else if(a > 50 && a <= 60) return list[4];
-    else {
-            setInterval(()=>{
-                callback(null, list[5]);
-            }, 1000);
-        };
-}
+MITASK-C 
 
-console.log("passed here 0");
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 
-maslahatBering(65, (err, data) => {
-    if(err) console.log('ERROR: ', err);
-    else {
-        console.log("javob: ", data);
+*/
+
+//MIT_TACK_C
+class Shop {
+    constructor(non, lagmon, cola){
+      this.non = non;
+      this.lagmon = lagmon;
+      this.cola = cola;
     }
-});
-
-//ASYNC FUNCTION
-
-// async function maslahatBering(a){
-//     if(typeof a !== "number") throw new Error("insert number");
-//     else if(a <= 20)  return list[0];
-//     else if(a > 20 && a <= 30) return list[1];
-//     else if(a > 30 && a <= 40) return list[2];
-//     else if(a > 40 && a <= 50) return list[3];
-//     else if(a > 50 && a <= 60) return list[4];
-//     else {
-//         return new Promise((resolve, reject) =>{
-//             setInterval(()=>{
-//                 resolve(list[5]);
-//             }, 1000)
-//         });
-
-
-//         // setTimeout(() => {
-//         //     callback(null, list[5]);
-//         // }, 5000);
-//     }
-// }
-
-
-// CALL VIA THEN/CATCH
-// maslahatBering(25).then(data => { //agar hatolik bo'lsa then ishlamaydi
-//     console.log('javob: ', data)
-// }).catch(err => {
-//     console.log('ERROR: ', err);
-// });
-
-// console.log('passed here 1');   
-
-//CALL VIA ASYNC AND AWAIT
-// async function run(){
-//     let javob = await maslahatBering(65);
-//     console.log(javob);
-//     javob = await maslahatBering(31);
-//     console.log(javob);
-//     javob = await maslahatBering(41);
-//     console.log(javob);
-// }
-// run();
-
-
-
-
-
-
-
+    
+    qoldiq(){
+      console.log(`${new Date().toTimeString().slice(0, 5)}da ${this.non}ta Non, ${this.lagmon}ta Lagmon va ${this.cola}ta Cola mavjud`);
+    }
+    
+    sotish(item, quantity){
+      console.log(`${new Date().toTimeString().slice(0, 5)}`);
+      this[item] += quantity;
+    }
+    
+    qabul(item, quantity){
+      console.log(`${new Date().toTimeString().slice(0, 5)}`);
+      this[item] += quantity;
+    }
+  }
+  
+  const shop = new Shop(4, 5, 2);
+  
+  shop.sotish('non', 3);
+  shop.qabul('cola', 4);
+  shop.qoldiq();
 
 
 
@@ -168,3 +121,115 @@ class Shop {
   
   */
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//**********TRAIN.JS CODE*********
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba bo'ling",
+//     "to'g'ri boshliq tanlang",
+//     "uzingizga ishlashni boshlang",
+//     "siz kuchli bolgan narsalarni qiling",
+//     "yoshlarga investitsiya qiling",
+//     "endi dam oling",
+// ];
+
+// //CALLBACK FUNCTION
+
+// function maslahatBering(a, callback){
+//     if(typeof a !== "number") callback("insert number", null);
+//     else if(a <= 20)  return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//             setInterval(()=>{
+//                 callback(null, list[5]);
+//             }, 1000);
+//         };
+// }
+
+// console.log("passed here 0");
+
+// maslahatBering(65, (err, data) => {
+//     if(err) console.log('ERROR: ', err);
+//     else {
+//         console.log("javob: ", data);
+//     }
+// });
+
+//ASYNC FUNCTION
+
+// async function maslahatBering(a){
+//     if(typeof a !== "number") throw new Error("insert number");
+//     else if(a <= 20)  return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) =>{
+//             setInterval(()=>{
+//                 resolve(list[5]);
+//             }, 1000)
+//         });
+
+
+//         // setTimeout(() => {
+//         //     callback(null, list[5]);
+//         // }, 5000);
+//     }
+// }
+
+
+// CALL VIA THEN/CATCH
+// maslahatBering(25).then(data => { //agar hatolik bo'lsa then ishlamaydi
+//     console.log('javob: ', data)
+// }).catch(err => {
+//     console.log('ERROR: ', err);
+// });
+
+// console.log('passed here 1');   
+
+//CALL VIA ASYNC AND AWAIT
+// async function run(){
+//     let javob = await maslahatBering(65);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

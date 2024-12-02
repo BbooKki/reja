98390aab10f5074c8a66b3a7264f3e0fb9c51424
Islom <karimov.islam5828859@gmail.com>
2024-12-03 01:05:34 +0900

    PORT was changed again to 6120 for PM2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongodb.connect(connectionString, {
 
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 6010;
+        let PORT = 6120;
         server.listen(PORT, function() {
             console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`)
         })

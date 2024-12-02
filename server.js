f220d9a3502070ca9fc0a1ff2e59fe9fc3ad2f91
Islom <karimov.islam5828859@gmail.com>
2024-12-03 00:52:41 +0900

    Port has been changed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongodb.connect(connectionString, {
 
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 6000;
+        let PORT = 6010;
         server.listen(PORT, function() {
             console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`)
         })
